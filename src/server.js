22916dbb85f8f3149629497efeb47e96140d8c8c
Joao Vinicius <joaovvs@gmail.com>
2023-11-14 12:12:44 -0300

    cors install

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("express-async-errors");
+const cors = require('cors')
 const express = require("express");
 const routes = require('./routes');
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
